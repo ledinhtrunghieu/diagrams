e73b862e65813bcb0d26c309e732a07334b393e2
ledinhtrunghieu <ledinhtrunghieuvt99@gmail.com>
2022-01-31 10:40:23 +0700

    update

--- diagram.py
+++ diagram.py
@@ -8,52 +8,64 @@ from diagrams.onprem.client import Client
 from diagrams.onprem.container import Docker
 from diagrams.onprem.workflow import Airflow
 from diagrams.onprem.client import Users
-
-from diagrams.aws.compute import ECS, EKS, Lambda
-from diagrams.aws.database import Redshift
-from diagrams.aws.integration import SQS
+from diagrams.generic.place import Datacenter
+from diagrams.programming.language import Python
+from diagrams.onprem.database import Postgresql
+from diagrams.onprem.analytics import Spark
+from diagrams.generic.storage import Storage
 from diagrams.aws.storage import S3
+from diagrams.aws.analytics import EMR
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import Redshift
+from diagrams.onprem.analytics import Metabase
+from diagrams.aws.network import ElbApplicationLoadBalancer
 
 
 
-with Diagram(name="Data Diagrams", show=True):
+
+with Diagram(name="Data Diagrams", show=True, direction="LR"):
     localdev = Client('Local Dev')
     docker = Docker('Docker')
     users = Users('Users/Data Consumers')
+
     with Cluster("Airflow control plane"):
         airflow = Airflow('Airflow')
+
         with Cluster("Local On Premises"):
-            with Cluster("Event Workers"):
-                workers = [ECS("worker1"),
-                        ECS("worker2"),
-                        ECS("worker3")]
+            local_rawdata = Datacenter("Raw Data")
 
-            queue = SQS("event queue")
+            with Cluster("Data Warehouse"):
+                dw_local_clean = Python("Pandas")
+                with Cluster("Column-Oriented Wrappers"):
+                    dw_local_postgresql = Postgresql('Postgre SQL')
+                local_rawdata >> dw_local_clean >> dw_local_postgresql
 
-            with Cluster("Processing"):
-                handlers = [Lambda("proc1"),
-                            Lambda("proc2"),
-                            Lambda("proc3")]
-
+            with Cluster("Data Lake"):
+                dw_local_spark = Spark("Spark")
+                dw_local_storage = Storage("Storage")
+                local_rawdata >> dw_local_spark >> dw_local_storage >> dw_local_postgresql
 
         with Cluster("AWS Cloud"):
-            with Cluster("Event Workers 2"):
-                workers2 = [ECS("worker1"),
-                        ECS("worker2"),
-                        ECS("worker3")]
+            cloud_s3 = S3("Amazon S3")
 
-            queue2 = SQS("event queue")
+            with Cluster("Data Warehouse"):
+                cloud_ec2 = EC2("AWS EC2") 
+                cloud_redshift = Redshift('AWS Redshift')
+                cloud_s3 >> cloud_ec2 >> cloud_redshift
 
-            with Cluster("Processing 2 "):
-                handlers2 = [Lambda("proc1"),
-                            Lambda("proc2"),
-                            Lambda("proc3")]
+            with Cluster("Data Lake"):
+                cloud_emr= EMR('AWS EMR')
+                cloud_bucket = S3("S3 Buckets")
+                cloud_s3 >> cloud_emr >> cloud_bucket >> cloud_redshift
 
-        store = S3("events store")
+        metabase =  Metabase("Metabase")
+        cloud_alb = ElbApplicationLoadBalancer("Application Load Balancer")
 
     localdev >> docker >> airflow
-    airflow >> workers >> queue >> handlers
-    airflow >> workers2 >> queue2 >> handlers2
-    handlers >> store
-    handlers2 >> store
-    store >> users
+    airflow >> cloud_s3
+    airflow >> local_rawdata
+    cloud_bucket >> metabase
+    cloud_redshift >> metabase
+    dw_local_storage >> metabase
+    dw_local_postgresql >> metabase
+    metabase >> cloud_alb >> users
